5a36f639966d7a277c312029a6b7d36d172850cb
ali <a.karamooz3232@gmail.com>
2026-08-15 21:19:23 +0330

    feat(blocks): add style-5 to ProductScroll for testing

---
+++ src/modules/home/components/ProductSplitView/ProductScroll/styles/style-5/index.tsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+export default function ProductScrollStyle5(props: any) {
+  return (
+    <div className="flex items-center justify-center min-h-[300px] bg-neutral-100 text-3xl font-bold rounded-xl border-4 border-dashed border-neutral-300 p-8 my-8 text-center text-black">
+      <p>تست استایل ۵ برای کامپوننت ProductScroll</p>
+    </div>
+  )
+}
